468f0ee51bbb6a87e3e68888f5b622c25a21b37f
bywangxp <bywangxp@mail.ustc.edu.cn>
2017-03-22 20:05:22 +0800

    用户信息接口

--- src/ustc/sse/water/docsearcher/controller/UserController.java
+++ src/ustc/sse/water/docsearcher/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import ustc.sse.water.docsearcher.dao.dao.PageDao;
 import ustc.sse.water.docsearcher.model.UserModel;
 import ustc.sse.water.docsearcher.service.ebi.DocumentEbi;
 import ustc.sse.water.docsearcher.service.ebi.UserEbi;
@@ -66,12 +67,6 @@ public class UserController {
 		}
 		temp.put("data", temp2);
 
-		/*
-		 * // 登录成功，返回主页 if (userModel != null) { session.setAttribute("user",
-		 * userModel); return "redirect:/index.html";// 登录成功去的页面，待修改 } else { //
-		 * 失败，返回错误信息，并返回到登录页面 session.setAttribute("errorinfo", "登录出错"); return
-		 * "redirect:/login.html"; }
-		 */
 		return temp;
 	}
 
@@ -150,5 +145,70 @@ public class UserController {
 		return totalmap;
 
 	}
-
+	// 用户主页基本信息显示
+		@ResponseBody
+		@RequestMapping(value = "/home", method = { RequestMethod.POST })
+		public Map<String, Object> getUserHome(HttpSession session) {
+			UserModel user =(UserModel) session.getAttribute("user");
+			HashMap<String, Object> map = new HashMap<String,Object>();
+			map.put("nickName", user.getUserNickName());
+			map.put("location", user.getUserLocation());
+			map.put("phone", user.getUserPhone());
+			map.put("email", user.getUserEmail());
+			map.put("description", user.getUserDescription());
+			map.put("userimage", user.getUserPhoto());
+			map.put("credit", user.getUserCredit());
+			//用户等级
+			map.put("level", 0);
+			map.put("docnum", user.getSumPublicDoc());//用户文档数
+			//用户评论数目，待添加功能
+			map.put("ratenum",0);
+			//获取收藏数据
+			map.put("collectnum", 0);
+			ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+			//图片信息
+			String []name=new String[4];
+			name[0]="本月上传文档";
+			name[1]="本月收藏文档";
+			name[2]="本月文档被评论数";
+			name[3]="平均分数";
+			String []data=new String[4];
+			//获取本月上传文档
+			data[0]="123,32,32,32,2,3,2,3,2,3,2,23";
+			//获取本月收藏文档
+			data[1]="123,32,32,32,2,3,2,3,2,3,2,23";
+			//本月文档被评论数
+			data[2]="123,32,32,32,2,3,2,3,2,3,2,23";
+			//平均分数
+			data[3]="123,32,32,32,2,3,2,3,2,3,2,23";
+			for(int i = 0 ; i < 4 ; ++i){
+				HashMap<String, Object> hash = new HashMap<String,Object>();
+				hash.put("name", name[i]);
+				hash.put("type", "bar");
+				String[] split = data[i].split(",");
+				hash.put("data", split);
+				list.add(hash);
+			}
+			map.put("series", list);
+			return map;
+		}
+		// 用户贡献的文档信息
+				@ResponseBody
+				@RequestMapping(value = "/contribution", method = { RequestMethod.POST })
+				public Map<String, Object> getContributionDoc(HttpSession session,HttpServletRequest request) {
+					String index = request.getParameter("index");//返回当前的页码值
+					if(index == null){
+						index = "1";
+					}
+					HashMap<String, Object> map = new HashMap<String,Object>();
+					//获取指定用户所有的文档
+					//获取到用户的信息
+					UserModel user =(UserModel) session.getAttribute("user");
+					int docNum=10;
+					map.put("totalpage",0 );//总共页面，每个页面显示十条数据
+				    ArrayList<String> list = new ArrayList<String>();
+//				    for(int i = 0; i < docNum / 10  ){
+//				    }
+					return null;
+				}
 }
